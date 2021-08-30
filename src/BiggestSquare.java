b1789fe99241b02ca643f93e9c5a4bd6325ebcf7
EP <kjuioqq8@gmail.com>
2021-08-30 22:07:26 +0900

    no complete

---
+++ src/BiggestSquare.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BiggestSquare {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] d = {2,2,3,3};
+
+        System.out.println(solution.solution(d,5));
+    }
+}
+
+class BiggestSquareSolution
+{
+    public int solution(int [][]board)
+    {
+        int answer = 1234;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
